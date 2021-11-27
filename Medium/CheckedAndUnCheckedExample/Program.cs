f8a7f87971d466b7eadc350af74b067a06bf9a12
serhatzor <serhat21zor@gmail.com>
2021-11-27 18:08:47 +0300

    Initial commit

---
+++ Medium/CheckedAndUnCheckedExample/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace CheckedAndUnCheckedExample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            PrintBigNumber();
+
+            checked
+            {
+                PrintBigNumber();
+            }
+
+            checked
+            {
+                int maxValueOfInt = 0;
+                unchecked
+                {
+                    maxValueOfInt = Int32.MaxValue;
+                    Console.WriteLine(maxValueOfInt);
+                    maxValueOfInt++;
+                    Console.WriteLine(maxValueOfInt);
+                }
+                maxValueOfInt = Int32.MaxValue;
+                maxValueOfInt++;
+            }
+        }
+
+        public static void PrintBigNumber()
+        {
+            int maxValueOfInt = Int32.MaxValue;
+            Console.WriteLine(maxValueOfInt);
+            maxValueOfInt = maxValueOfInt + 1;
+            Console.WriteLine(maxValueOfInt);
+        }
+    }
+}
+
+
